58e177213ffbb4ad3d6ad93b6bf7d3f11ebb7b6b
Venkat Polumahanti <venkat4541@gmail.com>
2017-04-11 09:44:28 -0400

    Event handling and binding in Angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div>
+  <app-simple-form></app-simple-form>
+  </div>
+  `,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <p>
-      simple-form Works!
-    </p>
+  <div>
+    <input #myInput type="text">
+    <button (mouseover)="onClick($event, myInput.value)">Click me!</button>
+  </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  onClick(ev, value) {
+    console.log(ev);
+    console.log(value);
+  }
 
   constructor() { }
 
